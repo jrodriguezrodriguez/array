d081fa9959d4471b3712fc27b334fe901f444fe5
Javier Rodriguez <Javier Rodriguez@192.168.1.228>
2015-01-21 18:08:29 +0100

    cambiada visibilidad atributo a private

--- array/src/ejerciciosArray/Colecciones1.java
+++ array/src/ejerciciosArray/Colecciones1.java
@@ -1,9 +1,18 @@
 package ejerciciosArray;
 
 public class Colecciones1 {
-	public int[] lista;
+	private int[] lista;
 
 	public Colecciones1() {
 		this.lista=new int[12];
 	}
+
+	public int[] getLista() {
+		return lista;
+	}
+
+	public void setLista(int[] lista) {
+		this.lista = lista;
+	}
+	
 }
--- array/src/ejerciciosArray/TestColecciones1.java
+++ array/src/ejerciciosArray/TestColecciones1.java
@@ -6,23 +6,23 @@ public class TestColecciones1 {
 		
 		//ejercicio1
 		System.out.println("Asigna el valor 5 al ultimo elemento:");
-		c1.lista[11]=5;
-		for (int i=0; i<c1.lista.length; i++){
-			System.out.print(c1.lista[i]+"\t");
+		c1.getLista()[11]=5;
+		for (int i=0; i<c1.getLista().length; i++){
+			System.out.print(c1.getLista()[i]+"\t");
 		}
 		System.out.println("\n");
 		
 		//ejercicio2
 		System.out.println("Asigna el resto de valores con un bucle que el primer elemento sea 3 y los restantes el doble del anterior:");
 		int contador=0;
-		for (int i=0; i<c1.lista.length; i++){
+		for (int i=0; i<c1.getLista().length; i++){
 			if (i==0){
-				c1.lista[i]=3;
-				System.out.print(c1.lista[i]+"\t");
+				c1.getLista()[i]=3;
+				System.out.print(c1.getLista()[i]+"\t");
 			}
 			else{
-				c1.lista[i]=c1.lista[contador]*2;
-				System.out.print(c1.lista[i]+"\t");
+				c1.getLista()[i]=c1.getLista()[contador]*2;
+				System.out.print(c1.getLista()[i]+"\t");
 				contador++;
 			}
 		}
@@ -31,30 +31,30 @@ public class TestColecciones1 {
 		//ejercicio3
 		System.out.println("Mediante un bucle calcula la suma de todos los elementos del array:");
 		int suma=0;
-		for (int i=0; i<c1.lista.length; i++){
-			suma+=c1.lista[i];
+		for (int i=0; i<c1.getLista().length; i++){
+			suma+=c1.getLista()[i];
 		}
 		System.out.println(suma);
 		System.out.println();
 		
 		//ejercicio4
 		System.out.println("Cambia el valor del array y divide por 3 todos los ultimo cinco valores:");
-		for (int i=0; i<c1.lista.length; i++){
+		for (int i=0; i<c1.getLista().length; i++){
 			if (i<7){
-				System.out.print(c1.lista[i]+"\t");
+				System.out.print(c1.getLista()[i]+"\t");
 			}else{
-			c1.lista[i]/=3;
-			System.out.print(c1.lista[i]+"\t");
+			c1.getLista()[i]/=3;
+			System.out.print(c1.getLista()[i]+"\t");
 			}
 		}
 		System.out.println("\n");
 		
 		//ejercicio5
 		System.out.println("Calcula ahora el valor mas pequeño almacenado en el array:");
-		int numeroMenor=c1.lista[0];
-		for (int i=0; i<c1.lista.length; i++){
-			if (numeroMenor>c1.lista[i]){
-				numeroMenor=c1.lista[i];
+		int numeroMenor=c1.getLista()[0];
+		for (int i=0; i<c1.getLista().length; i++){
+			if (numeroMenor>c1.getLista()[i]){
+				numeroMenor=c1.getLista()[i];
 			}
 		}
 		System.out.println(numeroMenor);
@@ -63,12 +63,12 @@ public class TestColecciones1 {
 		//ejercicio6
 		System.out.println("Muestra todos los valores en tres filas de cuatro columnas, con una separacion de cinco espacios:");
 		int c=0;
-		for (int i=0; i<c1.lista.length; i++){
+		for (int i=0; i<c1.getLista().length; i++){
 			c++;
 			if (c<3){
-				System.out.print(c1.lista[i]+"\t");
+				System.out.print(c1.getLista()[i]+"\t");
 			}else{
-				System.out.print(c1.lista[i]+"\t");
+				System.out.print(c1.getLista()[i]+"\t");
 				System.out.println();
 				c=0;
 			}
